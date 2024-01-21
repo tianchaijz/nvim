a9c3d064811590911695e7050d86e2d6c9c5a3e9
Jinzheng Zhang <tianchaijz@gmail.com>
2024-01-21 12:59:25 +0000

    Feature: add `nvim-autopairs`

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -146,4 +146,39 @@ return {
       cmp.setup(opts)
     end,
   },
+
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {
+      check_ts = true,
+      ts_config = { java = false },
+      fast_wrap = {
+        map = "<M-e>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+        offset = 0,
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "PmenuSel",
+        highlight_grey = "LineNr",
+      },
+    },
+    config = function(_, opts)
+      local autopairs_enabled = true
+      local npairs = require("nvim-autopairs")
+      npairs.setup(opts)
+      if not autopairs_enabled then npairs.disable() end
+      local cmp_status_ok, cmp = pcall(require, "cmp")
+      if cmp_status_ok then
+        cmp.event:on(
+          "confirm_done",
+          require("nvim-autopairs.completion.cmp").on_confirm_done({
+            tex = false,
+          })
+        )
+      end
+    end,
+  },
 }
